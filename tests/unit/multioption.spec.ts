abeb215bb5ecbb74a6acfc528177bbc05759d9ff
chenpan1@sfmail.sf-express.com <chenpan1@sfmail.sf-express.com>
2020-02-05 09:55:39 +0800

    select的单元测试

--- tests/unit/multioption.spec.ts
+++ tests/unit/multioption.spec.ts
@@ -2,29 +2,34 @@ import { createLocalVue, shallowMount, mount } from '@vue/test-utils';
 import multiOption from '../../packages/multi-option/src/main.vue';
 
 describe('multiOption.vue', () => {
-  it('测试props', () => {
-    const value = '选项1';
-    const label = '黄金糕';
-    const wrapper = shallowMount(multiOption, {
+  let wrapper: any;
+  const value = '选项1';
+  const label = '黄金糕';
+  beforeEach(() => {
+    wrapper = shallowMount(multiOption, {
       propsData: { value, label },
     });
+  });
+  it('测试props', () => {
     expect(wrapper.text()).toContain(label);
+    wrapper.destroy();
   });
   it('测试data', () => {
-    const wrapper = shallowMount(multiOption);
-    // tslint:disable-next-line: no-console
     expect(wrapper.vm.$data.selected).toEqual(false);
+    expect(wrapper.vm.$data.selected).toEqual(false);
+    expect(wrapper.vm.$data.selected).toEqual(false);
+    wrapper.destroy();
   });
   it('测试方法', async () => {
-    const wrapper = mount(multiOption);
     const handleInitSelect = await (wrapper.vm as any).handleInitSelect();
     expect(handleInitSelect).toBe(undefined);
+    wrapper.destroy();
   });
   it('测试事件', () => {
     const stub = jest.fn(); // 伪造一个jest的mock funciton
-    const wrapper = mount(multiOption);
     wrapper.setMethods({ handleClick: stub }); // setMethods将handleClick这个方法覆写
     wrapper.find('li').trigger('click'); // 对li触发一个click事件
     expect(stub).toBeCalled(); // 查看handleClick是否被调用
+    wrapper.destroy();
   });
 });
